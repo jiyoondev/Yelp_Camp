3ba92e64ccc92f22eb94f7cad1a2ccc43648f5ea
jiyoondev <jiyoon.jang.dev@gmail.com>
2021-05-08 07:15:09 +0900

    Create CRUD operation_update, delete

--- app.js
+++ app.js
@@ -44,6 +44,25 @@ app.get('/campgrounds/:id', async (req, res) => {
 });
 //:id route를 new route 위에 써버리면 new를 id로 취급하므로 순서가 중요함
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
+});
+
 app.listen(3000, () => {
   console.log('serving on port 3000');
 });
