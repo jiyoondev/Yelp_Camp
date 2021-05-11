c12ee3931f55e159054466ead9312f5d56cece4a
jiyoondev <jiyoon.jang.dev@gmail.com>
2021-05-11 20:38:57 +0900

    Finish Basic layout and style using bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collections/11536948/camping',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in urna eu diam convallis consequat. Sed nec sem dignissim, lacinia diam sed, congue arcu. Morbi commodo lobortis neque ac semper. Donec non mauris ut sem mattis consequat.',
+      price,
     });
     await camp.save();
   }
